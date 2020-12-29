63235c94d44a63a32a51f05263ec6551e87e023b
Santino <santi.barto12@gmail.com>
2020-12-29 12:22:43 -0300

    Actualizacion index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ client.on('message', message => {
             message.reply('Porfavor introduzca solo numeros luego del comando');
         }
         else{
-            setTimeout(contador,timeToWait * 60000);
+            setTimeout(contador,timeToWait);
             message.channel.send('Contador iniciado');
         }
     }
@@ -55,6 +55,19 @@ client.on('message', message => {
               .setThumbnail(message.author.displayAvatarURL())
               .setTimestamp()
               .addField("Comandos", "/contador* tiempo * : ,iniciar contador en minutos")
+    
+        message.channel.send({ embed: embedDatos });
+    }
+
+    //Lista de Milisegundos a Minutos
+    if (message.content === '/help'){
+        const embedDatos = new Discord.MessageEmbed() 
+              .setTitle("Tabla de equivalencias")
+              .setColor(0x00AE86)
+              .setFooter("Cualquier duda o problema contactarse con RyZe", client.user.avatarURL())
+              .setThumbnail(message.author.displayAvatarURL())
+              .setTimestamp()
+              .addField("Equivalencias", "1 minuto = 60000\n10 minutos = 600000\n30 minutos = 1800000\n45 minutos = 2700000\n60 minutos = 3600000\nCulaquier otro minuto hagan, X * 60000")
     
         message.channel.send({ embed: embedDatos });
     }
