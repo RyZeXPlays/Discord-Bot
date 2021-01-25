e4e1aaffeed04b528863b775438a7f5171b7bda8
Santino <santi.barto12@gmail.com>
2021-01-25 00:20:56 -0300

    Organizando comandos y nuevos comandos

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const Discord = require('discord.js');
-const BombSweeper = require("bombsweeper.js");
 const client = new Discord.Client();
+const config = require("./config.json");
 
+const dadosImage = ["https://cdn.discordapp.com/attachments/684757256658747451/794277079243685888/dado-1.png", "https://cdn.discordapp.com/attachments/684757256658747451/794277107537805332/dado-2.png", "https://cdn.discordapp.com/attachments/684757256658747451/794277142800105483/dado-3.png", "https://cdn.discordapp.com/attachments/684757256658747451/794277176592826368/dado-4.png", "https://cdn.discordapp.com/attachments/684757256658747451/794277207619010590/dado-5.png", "https://cdn.discordapp.com/attachments/684757256658747451/794277245157113866/dado-6.png"]
+
+let prefix = config.prefix;
 
 function numero_al_azar(minimo,maximo){
-        return Math.floor(Math.random() * (maximo - minimo + 1) + minimo); 
+    return Math.floor(Math.random() * (maximo - minimo + 1) + minimo); 
 }
 
 client.on('ready', () => {
@@ -18,24 +21,44 @@ client.on('ready', () => {
      });
 });
 
-client.on('message', message => {
 
-    //Receiving the message
-    if (message.content === '/ping'){
-        let ping = Math.floor(message.client.ws.ping);
-        message.channel.send('Pong, ' + ping + 'ms');
-    }
+client.on('message', (message) => {
+    //Evitar la respuestas entre bots
+    if (!message.content.startsWith(prefix)) return; 
+    if (message.author.bot) return;
 
-    if (message.content === '/hola'){
-        message.channel.send(`Hola ${message.author}`);
-    }
+    const args = message.content.slice(prefix.length).split(/ +/g);
+    const command = args.shift().toLowerCase();
 
-    if (message.content === '/informe'){
+    //Roles
+    let rol = message.guild.roles.cache.find(r => r.name === "BotDeveloper");
+    let permiso = message.member.hasPermission("ADMINISTRATOR");
+    //Rol admin en el server Ghouls
+    let rolAdmin = message.guild.roles.cache.find(r => r.name === "Admin")
+
+    if (command === "informe"){
+        if (rol || permiso || rolAdmin){
             message.channel.send(`Acá laburando para levantar el pais en ${client.guilds.cache.size} servers, con ${client.users.cache.size} usuarios`);
-    }   
+        }
+    }
+
+    //Eliminacion masiva de mensajes
+    if (command === "purge"){
+        if (rol || permiso || rolAdmin){
+            let cantidad = parseInt(args[0]);
+            message.channel.bulkDelete(cantidad);
+            if (!cantidad){message.channel.send("No ingresó la cantidad")}
+        }
+    }
+
+    //Comprobar respuesta y tiempo de respuesta del bot
+    if(command === "ping"){
+        let ping = Math.floor(message.client.ws.ping);
+        message.channel.send('Pong :ping_pong:, ' + ping + 'ms');
+    }
 
     //Contador
-    if (message.content.startsWith('/contador')){
+    if (command === "contador"){
         //Tomamos el los caracteres despues del comando y el espacio
         timeToWait = message.content.slice(10);
         //Comprobamos con isNan si no es un numero
@@ -47,29 +70,29 @@ client.on('message', message => {
             message.channel.send('Contador iniciado');
         }
     }
-
     function contador() {
         message.reply('Ya puedes tirar de la ruleta'); 
     }
-     
+    
     //Comando ayuda
-    if (message.content === '/help'){
+    if (command === "help"){
         const embedDatos = new Discord.MessageEmbed() 
               .setTitle("Ayuda")
-              .setColor(0x00AE86)
+              .setColor("RANDOM")
               .setFooter("Cualquier duda o problema contactarse con RyZe", client.user.avatarURL())
               .setThumbnail(message.author.displayAvatarURL())
               .setTimestamp()
-              .addField("Comandos", "/contador ´tiempo´: iniciar contador en milisegundos.\n/equivalencias: lista de equivalencias y formula de milisegundos a minutos.\n/8ball ´Pregunta´: Haga una pregunta y el bot respondera.\n/random: genera un numero aleatorio comprendido entre 0 y 100.\n/say ´Texto´: manda un mensaje utilizando al bot como emisor de este.")
+              .setDescription("Agregue el prefijo `/` antes de cualquier comando")
+              .addField("Comandos", "`contador 'tiempo':` iniciar contador en milisegundos.\n`equivalencias:` lista de equivalencias y formula de milisegundos a minutos.\n`8ball: 'Pregunta':` Haga una pregunta y el bot respondera.\n`random:` genera un numero aleatorio comprendido entre 0 y 100.\n`say 'Texto'`: manda un mensaje utilizando al bot como emisor de este.\n`dice:` Tira un dado de 6 caras")
     
         message.channel.send({ embed: embedDatos });
     }
 
     //Lista de Milisegundos a Minutos
-    if (message.content === '/equivalencias'){
+    if (command === "equivalencias"){
         const embedDatos = new Discord.MessageEmbed() 
               .setTitle("Tabla de equivalencias")
-              .setColor(0x00AE86)
+              .setColor("RANDOM")
               .setFooter("Cualquier duda o problema contactarse con RyZe", client.user.avatarURL())
               .setThumbnail(message.author.displayAvatarURL())
               .setTimestamp()
@@ -79,7 +102,7 @@ client.on('message', message => {
     }
     
     //Comando 8ball
-    if (message.content.startsWith("/8ball")){
+    if (command === "8ball"){
         var rpts = ["Si­", "No", "Tal Vez", "Nunca", "No lo se", "Muy poco probable", "Ambos sabemos que no", "100.000% seguro que si", "Te diría que no pero la verdad es que si"]; // Las Respuestas
         let question = message.content;
 
@@ -91,111 +114,56 @@ client.on('message', message => {
             .setThumbnail(message.author.avatarURL)
             .addField('Su Pregunta:', pregunt)
             .addField('Mi respuesta es:', rpts[Math.floor(Math.random() * rpts.length)])
+            .setColor("RANDOM")
 
         message.channel.send(embed);
     }
 
-    if (message.content === "/random"){
+    //Numero aleatorio
+    if (command === "random"){
         let randomN = numero_al_azar(0,100);
         message.channel.send(`El numero es ${randomN}`);
     }
 
-    if (message.content.startsWith("/say ")){
+    //Say
+    if (command === "say"){
         let msg = message.content.slice(5)
         message.channel.send(msg);
         message.delete();
     }
 
+    //Tirar dados
+    if (command === "dice"){
+        var dado = dadosImage[Math.floor(Math.random() * dadosImage.length)]
+        const embedDice = new Discord.MessageEmbed()
+        .setTitle(`${message.author.username} ha tirado el dado.`)
+        .setDescription("El dado a caido en:")
+        .setImage(dado)
+        .setColor("RANDOM")
+        message.channel.send(embedDice);
+    }
+});
+
+client.login(config.token);
+
+
+
+
+
+
+
+
+
+
     
-/*if (message.content === "/play Buscaminas" || message.content === "/play buscaminas"){
-        Asnwered = false;
-        AsnweredDificult = false;
-        
-        message.channel.send("Estas listo? Responde con `yes` o `no`")
+/* Ejemplo de collector
+
         const filter = m => m.content === 'yes' || m.content === 'no' && m.author.id === message.author.id;
         const collector = message.channel.createMessageCollector(filter, { time: 15000 });
         
         collector.on('collect', m => {
             if (m.content === 'yes'){
                 Asnwered = true;
-                
-                message.channel.send("Elige la dificultad: `facil`, `medio`, `dificil`")
-                const filter = i => i.content === 'facil' || i.content === 'medio' || i.content === 'dificil' && i.author.id === message.author.id;
-                const collector = message.channel.createMessageCollector(filter, { time: 15000 }); 
-                
-                collector.on('collect', i =>{
-                    if (i.content === 'facil'){
-                        //Tablero
-                        let filas = 10;
-                        let cols = 10;
-                        const bombsweeper = new BombSweeper(filas, cols);
-                        //Bombas
-                        let bombas = 10;
-                        bombsweeper.PlaceBombs(bombas);
-                        
-                        let tablero = bombsweeper.board;
-                    
-                        for (let j = 0; j<10; j++){
-                            for (let i = 0; i<10; i++){
-                                tablero[i][j] = emojis[tablero[i][j]];
-                            }
-                        }
-                        //Juego listo
-                        bombsweeper.start();
-                    }
-                    else if (i.content === 'medio'){
-                        //Tablero
-                        let filas = 14;
-                        let cols = 18;
-                        const bombsweeper = new BombSweeper(filas, cols);
-                        //Bombas
-                        let bombas = 40;
-                        bombsweeper.PlaceBombs(bombas);
-
-                        let tablero = bombsweeper.board;
-                    
-                        for (let j = 0; j<10; j++){
-                            for (let i = 0; i<10; i++){
-                                tablero[i][j] = emojis[tablero[i][j]];
-                            }
-                        }
-                        //Juego listo
-                        message.channel.send(tablero);
-                    }
-                    else if (i.content === 'dificil'){
-                        //Tablero
-                        let filas = 20;
-                        let cols = 24;
-                        const bombsweeper = new BombSweeper(filas, cols);
-                        //Bombas
-                        let bombas = 99;
-                        bombsweeper.PlaceBombs(bombas);
-
-                        let tablero = bombsweeper.board;
-                    
-                        for (let j = 0; j<10; j++){
-                            for (let i = 0; i<10; i++){
-                                tablero[i][j] = emojis[tablero[i][j]];
-                            }
-                        }
-                        //Juego listo
-                        message.channel.send(tablero);
-                    }
-                    AsnweredDificult = true;
-                });
-
-                collector.on('end', collected =>{
-                   if (AsnweredDificult == true){
-                       return;
-                   }
-                   else(message.reply("Se acabó el tiempo"))
-                })
-            }
-            else if (m.content === 'no'){
-                Asnwered = true;
-                message.reply("Se cancelo el juego");
-            }
-        });
         
         collector.on('end', collected => {
             if (Asnwered == true){
@@ -204,6 +172,3 @@ client.on('message', message => {
             else{message.reply("Se acabó el tiempo")}
         });
     }*/
-}); 
-
-client.login('NzkzMzMyMjk5NjUwMzAxOTYz.X-quRw.FjjIpt4ClmEiTuuh3lXRveX5KT4');
